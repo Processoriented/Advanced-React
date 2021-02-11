17132dd19e6f06f41a14625ace4a21ac92eb557a
Vincent Engler <vincent.engler@mavenwave.com>
2021-02-11 16:55:57 -0600

    added useForm hook

---
+++ sick-fits/frontend/components/CreateProduct.js
@@ -0,0 +1,43 @@
+import useForm from '../lib/useForm';
+import Form from './styles/Form';
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    name: 'Nice Shoes',
+    price: 34624,
+    description: 'These are the best shoes.',
+  });
+  return (
+    <Form>
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input type="file" id="image" name="image" onChange={handleChange} />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
+  );
+}
---
+++ sick-fits/frontend/lib/useForm.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+    if (type === 'number') {
+      value = parseInt(value, 10);
+    }
+    if (type === 'file') {
+      [value] = e.target.files;
+    }
+    setInputs({
+      // copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.keys(inputs).reduce(
+      (rtn, key) => ({ ...rtn, [key]: '' }),
+      {}
+    );
+    setInputs(blankState);
+  }
+
+  // return what we want to surface
+  return { inputs, handleChange, resetForm, clearForm };
+}
