98996089e1863062758d0737b348fd2efdda12ca
Vincent Engler <vincent.engler@mavenwave.com>
2021-02-23 13:07:23 -0600

    loop afterthoughts lint rule

--- sick-fits/frontend/lib/paginationField.js
+++ sick-fits/frontend/lib/paginationField.js
@@ -4,7 +4,6 @@ export default function paginationField() {
   return {
     keyArgs: false, // tells apollo we will handle things
     read(existing = [], { args, cache }) {
-      console.log({ existing, args, cache });
       const { skip, first } = args;
       const data = cache.readQuery({ query: PAGINATION_QUERY });
       const count = data?._allProductsMeta?.count;
@@ -14,7 +13,6 @@ export default function paginationField() {
       // Check the cache and return, or false will make api call
       const items = existing.slice(skip, skip + first).filter((x) => x);
       if (items.length && items.length !== first && page === pages) {
-        console.log('here');
         return items;
       }
       if (items.length !== first) {
@@ -23,9 +21,6 @@ export default function paginationField() {
       }
 
       if (items.length) {
-        console.log(
-          `There are ${items.length} items in the cache!  Gonna send them to apollo`
-        );
         return items;
       }
 
@@ -34,13 +29,10 @@ export default function paginationField() {
     merge(existing, incoming, { args }) {
       const { skip, first } = args;
       // Runs after api call
-      console.log(`Merging items from the network ${incoming.length}`);
       const merged = existing ? existing.slice(0) : [];
-      // eslint-disable-next-line no-plusplus
-      for (let i = skip; i < skip + incoming.length; ++i) {
+      for (let i = skip; i < skip + incoming.length; i++) {
         merged[i] = incoming[i - skip];
       }
-      console.log(merged);
       return merged;
     },
   };
